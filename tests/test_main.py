532e3e37285f61e0a5c6e8fb74dd49501dc37b82
Sirish Kumar <sirishk99@gmail.com>
2025-12-28 12:36:55 +0000

    test script error fixed

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,11 @@
 import unittest
 import sys
-sys.path.insert(0, '../src')
+import os
 
-from main import app
+# Add the parent directory to the path to import from src
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.main import app
 
 
 class TestMainApp(unittest.TestCase):
