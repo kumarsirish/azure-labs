a34c57dc7ce197a26ba349d871f1667bf28e0a56
Sirish Kumar <sirishk99@gmail.com>
2026-01-22 15:56:06 +0000

    changes done

--- src/app.py
+++ src/app.py
@@ -7,5 +7,6 @@ def hello():
     return "<h1>Congratulations!<br>Your application is running in Azure Cloud</h1>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
 
